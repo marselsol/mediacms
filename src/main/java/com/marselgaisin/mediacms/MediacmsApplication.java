3ff13df82d64e2e6df733c243ccbb03884fd3b4e
Marsel Gaisin <marselgaisin@gmail.com>
2025-12-16 22:09:56 +0300

    first commit

---
+++ src/main/java/com/marselgaisin/mediacms/MediacmsApplication.java
@@ -0,0 +1,13 @@
+package com.marselgaisin.mediacms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediacmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediacmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marselgaisin/mediacms/MediacmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marselgaisin.mediacms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediacmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
